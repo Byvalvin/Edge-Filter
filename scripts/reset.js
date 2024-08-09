f83e505b49528779a73637aadb50780c2767dcd7
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-08 18:09:09 -0600

    Update reset.js

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,20 +1,17 @@
 // Function for reset button
 const buttonCode = `
-const resetControl = () => {
-  const resetButton = document.getElementById('resetButton') as HTMLButtonElement;
-    resetButton.addEventListener('click', () => {
-        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-        const ctx = canvas.getContext('2d');
-        
-        // Clear the canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
-        // Reset dropdown to default (Sobel)
-        const edgeDetectionMethod = document.getElementById('edgeDetectionMethod') as HTMLSelectElement;
-        edgeDetectionMethod.value = 'sobel'; // Reset to default option
-    });
-  
-};
+const resetButton = document.getElementById('resetButton') as HTMLButtonElement;
+  resetButton.addEventListener('click', () => {
+      const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+      const ctx = canvas.getContext('2d');
+      
+      // Clear the canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
+      // Reset dropdown to default (Sobel)
+      const edgeDetectionMethod = document.getElementById('edgeDetectionMethod') as HTMLSelectElement;
+      edgeDetectionMethod.value = 'sobel'; // Reset to default option
+  });
 `
 // Evaluate the TypeScript code and run it
 const buttonJsCode = ts.transpile(buttonCode);
