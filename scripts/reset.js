f429677eb1867757e2cb91b2d12475e44de31e7d
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-08 21:06:05 -0600

    Update reset.js

--- scripts/reset.js
+++ scripts/reset.js
@@ -10,13 +10,13 @@ const uploadInput = document.getElementById('upload') as HTMLInputElement;
 
 if (resetButton && canvas && edgeDetectionMethod && uploadInput && sobelCanvas && cannyCanvas) {
     resetButton.addEventListener('click', () => {
-        const ctx = canvas.getContext('2d');
+        // const ctx = canvas.getContext('2d');
         const sobelCtx = sobelCanvas.getContext('2d');
         const cannyCtx = cannyCanvas.getContext('2d');
 
         if (ctx && sobelCtx && cannyCtx) {
             // Clear all canvas elements
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            // ctx.clearRect(0, 0, canvas.width, canvas.height);
             sobelCtx.clearRect(0, 0, sobelCanvas.width, sobelCanvas.height);
             cannyCtx.clearRect(0, 0, cannyCanvas.width, cannyCanvas.height);
         }
